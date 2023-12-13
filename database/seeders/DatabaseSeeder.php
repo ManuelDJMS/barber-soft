fe1475f34054f1a98a6d57c7e9ecb2e0a0686132
Manuel Morales <46793411+ManuelDJMS@users.noreply.github.com>
2023-12-13 16:32:03 -0600

    Create Seeder Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Manuel',
+            'last_name' => 'Morales',
+            'address' => 'Roble 4',
+            'phone' => '3411671215',
+            'name' => 'Test User',
+            'email' => 'admin@admin.com',
+            'password'=> Hash::make('123456')
+        ]);
     }
 }
